9a175c53f329511a1f4f1aa4d9b7656973d14ec1
tanjib10 <radiulmd@gmail.com>
2024-01-11 14:11:36 +0600

    added auth provider

---
+++ src/components/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import app from "../../../Firebase/firebase.config";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+export const AuthContext = createContext({});
+const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const axiosPublic = useAxiosPublic();
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   const logInGoogle = (auth) => {
+  //     return signInWithPopup(auth, provider);
+  //   };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      // eslint-disable-next-line no-empty
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+        // eslint-disable-next-line no-empty
+      } else {
+        localStorage.removeItem("access-token");
+      }
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, [axiosPublic]);
+
+  const authInfo = {
+    user,
+    createUser,
+    //  logInGoogle,
+    signIn,
+    logOut,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
